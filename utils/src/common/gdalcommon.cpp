6c1d5b8b169d2a6e87cc9015fc42ceeb221e4f56
Pete Bunting <petebunting@mac.com>
2012-08-13 14:43:11 +1200

    Update to check whether a colour table existing for thematic data and creates one if one does not exist. This is because erdas imagine expects one to be present.

--- utils/src/common/gdalcommon.cpp
+++ utils/src/common/gdalcommon.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string>
 #include <iostream>
+#include <ctime>
 
 #include "gdal.h"
 #include "cpl_string.h"
@@ -712,6 +713,28 @@ void calcstats( GDALDataset *hHandle, bool bIgnore, float fIgnoreVal, bool bPyra
      correctly in Imagine */
     GDALSetMetadataItem( hBand, "STATISTICS_HISTOBINFUNCTION", histoType.c_str(), NULL );
     
+    if(strcmp( hBand->GetMetadataItem( "LAYER_TYPE", "" ), "thematic" ) == 0)
+    {
+        GDALColorTable *clrTab = hBand->GetColorTable();
+        if(clrTab == NULL)
+        {
+            std::cout << "Adding a Colour table\n";
+            clrTab = new GDALColorTable();
+            GDALColorEntry *clr = NULL;
+            srand(time(NULL));
+            for(size_t i = 0; i < histmax; ++i)
+            {
+                clr = new GDALColorEntry();
+                clr->c1 = rand() % 255 + 1;
+                clr->c2 = rand() % 255 + 1;
+                clr->c3 = rand() % 255 + 1;
+                clr->c4 = 255;
+                clrTab->SetColorEntry(i, clr);
+            }
+            hBand->SetColorTable(clrTab);
+        }
+    }
+      
     delete pszHistoString;
     free( pHisto );
   }
